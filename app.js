24bbcb6b778874a482a90feb40c284b09e5f6809
Bettyk <abtechbsolutions@gmail.com>
2018-08-10 18:32:47 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,9 +2,10 @@
  * Create a list that holds all of your cards
  */
 
-const icons = ["fa fa-diamond", "fa fa-paper-plane-o", "fa fa-anchor", "fa fa-bolt", "fa fa-cube", "fa fa-anchor", "fa fa-leaf",
-                "fa fa-bicycle", "fa fa-diamond", "fa fa-bomb", "fa fa-leaf", "fa fa-bomb", "fa fa-bolt", "fa fa-bicycle", 
-                "fa fa-paper-plane-o", "fa fa-cube"];
+const icons = ["fa fa-diamond",
+"fa fa-paper-plane-o", "fa fa-anchor", "fa fa-bolt", "fa fa-cube", "fa fa-anchor", "fa fa-leaf", "fa fa-bicycle", "fa fa-diamond",
+"fa fa-bomb", "fa fa-leaf", "fa fa-bomb", "fa fa-bolt", "fa fa-bicycle", "fa fa-paper-plane-o", "fa fa-cube"];
+
 //The Deck container
 const cardDeck =
       document.querySelector(".deck");
