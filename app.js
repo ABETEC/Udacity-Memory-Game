fc8cd3991e1968bcb4a7da907a35cafb354eb795
Bettyk <abtechbsolutions@gmail.com>
2018-08-10 17:15:29 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 /*
  * Create a list that holds all of your cards
  */
-,const icons = ["fa fa-diamond","fa fa-paper-plane-o", "fa fa-anchor", "fa fa-bolt", "fa fa-cube", "fa fa-anchor", "fa fa-leaf",
-                "fa fa-bicycle", "fa fa-diamond", "fa fa-bomb", "fa fa-leaf", "fa fa-bomb",  "fa fa-bolt", "fa fa-bicycle", 
+,const icons = ["fa fa-diamond", "fa fa-paper-plane-o", "fa fa-anchor", "fa fa-bolt", "fa fa-cube", "fa fa-anchor", "fa fa-leaf",
+                "fa fa-bicycle", "fa fa-diamond", "fa fa-bomb", "fa fa-leaf", "fa fa-bomb", "fa fa-bolt", "fa fa-bicycle", 
                 "fa fa-paper-plane-o", "fa fa-cube"];
 
 /*
